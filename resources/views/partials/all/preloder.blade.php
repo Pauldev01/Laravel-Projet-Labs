c8b74026c87ed0bcb2f58666dcababe09302a33e
Okyop <pcombes80@gmail.com>
2021-04-06 16:58:20 +0200

    fin lte

--- resources/views/partials/all/preloder.blade.php
+++ resources/views/partials/all/preloder.blade.php
@@ -2,6 +2,6 @@
 <div id="preloder">
 	<div class="loader">
 		<img src="img/logo.png" alt="">
-		<h2>Loading.....</h2>
+		<h2>Loading....</h2>
 	</div>
 </div>
--- resources/views/template/main.blade.php
+++ resources/views/template/main.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Labs - Design Studio</title>
+    <title>Projet Labs Paul Bossaert</title>
     @include('partials.all.link')
 </head>
 <body>
